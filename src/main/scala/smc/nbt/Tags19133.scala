caff64901b77c733ffb468b04c1a984ce0752169
ryo0ka <ryoka0418@gmail.com>
2016-01-18 18:04:16 -0800

    fixed error

--- src/main/scala/smc/nbt/Tags19133.scala
+++ src/main/scala/smc/nbt/Tags19133.scala
@@ -9,6 +9,11 @@ import scala.reflect.runtime.universe._
   */
 object Tags19133 extends Tags with TagMaps with TagArrays with TagsOps with DirtyEnum {
 
+
+  protected def impl[A: TypeTag](gp: GetPut[A]) = new TypeDefImpl[A] {
+    override val getput = gp
+    override val typ = typeTag[A]
+  }
   override type TypeDef[A] = TypeDefImpl[A]
   implicit override val EndDef = new TypeDefImpl[End.type] with End
   implicit val ByteDef = impl(GetPutByte)
@@ -22,11 +27,6 @@ object Tags19133 extends Tags with TagMaps with TagArrays with TagsOps with Dirt
   implicit val ArrayDef = impl(GetPutTagArray)
   implicit val MapDef = impl(GetPutTagMap)
   implicit val IntsDef = impl(new GetPutArray(GetPutInt))
-
-  protected def impl[A: TypeTag](gp: GetPut[A]) = new TypeDefImpl[A] {
-    override val getput = gp
-    override val typ = typeTag[A]
-  }
 
   protected trait TypeDefImpl[A] extends TypeDefAbs[A] with DirtyElement
 }
